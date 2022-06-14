53afac2a184c3ed686f79693d60010022297fc30
Vishnu Ramasubramanian <ivanvishnu@gmail.com>
2022-06-14 07:40:02 -0500

    Update install.r

--- install.r
+++ install.r
@@ -1,22 +1,29 @@
+install.packages("rlang")
+install.packages("dplyr")
 install.packages("rrBLUP")
 install.packages("vcfR")
-install.packages("dplyr")
+
 install.packages("NAM")
 install.packages("bWGR")
 install.packages("STPGA")
 install.packages("BGLR")
 install.packages("SOMMER")
 
- install.packages("BiocManager")
-  BiocManager::install(version = "3.14")
+install.packages("BiocManager")
+BiocManager::install(version = "3.14")
  
-  options(repos = BiocManager::repositories())
-  library(BiocManager)
+options(repos = BiocManager::repositories())
+library(BiocManager)
+install.packages("rJava")
 
- install.packages("devtools")
- library(devtools)
- devtools::install_bitbucket(
+install.packages("devtools")
+library(devtools)
+devtools::install_bitbucket(
 		repo = "bucklerlab/rTASSEL",
 		ref = "master",
 		build_vignettes = FALSE
- ) 
+) 
+
+
+
+
